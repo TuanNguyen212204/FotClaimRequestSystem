699be964376525a0a25623994c3f38a7e2135d9a
15VuNguyen <146836992+15VuNguyen@users.noreply.github.com>
2025-02-14 15:35:45 +0700

    Vu Nguyen commit login feature

--- Desktop/claim_request_FE/src/routers/index.tsx
+++ Desktop/claim_request_FE/src/routers/index.tsx
@@ -4,24 +4,34 @@ import { HomePage } from "../pages/HomePage";
 import { PATH } from "../constant/config";
 import DraftCoponent from "../components/ui/user/DraftCoponent";
 import { UserInfoComponent } from "../components/ui/user/UserInfoComponent";
+import LoginForm from "../components/ui/login/LoginForm";
+import ResetPassword from "../components/ui/login/ResetPassword";
 
 const router: RouteObject[] = [
-    {
-        element: <MainLayout />,
-        children: [
-            {
-                path: PATH.home,
-                element: <HomePage />,
-            }, 
-            {
-                path: PATH.draft,
-                element: <DraftCoponent />
-            },
-            {
-                path: PATH.userinfo,
-                element: <UserInfoComponent />
-            }
-        ]
-    }
-]
-export const Router = () => useRoutes(router)
+  {
+    element: <LoginForm />,
+    path: PATH.login,
+  },
+  {
+    element: <ResetPassword />,
+    path: PATH.resetPassword,
+  },
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: PATH.home,
+        element: <HomePage />,
+      },
+      {
+        path: PATH.draft,
+        element: <DraftCoponent />,
+      },
+      {
+        path: PATH.userinfo,
+        element: <UserInfoComponent />,
+      },
+    ],
+  },
+];
+export const Router = () => useRoutes(router);
