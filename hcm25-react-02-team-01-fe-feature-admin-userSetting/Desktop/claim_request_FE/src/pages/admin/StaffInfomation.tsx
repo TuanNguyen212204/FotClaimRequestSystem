05fdc19266fc301c8c0a0f7049804ff49dbe8e9c
AnhKiet <bucash7@gmail.com>
2025-02-15 15:57:32 +0700

    Kiet Admin

---
+++ hcm25-react-02-team-01-fe-feature-admin-userSetting/Desktop/claim_request_FE/src/pages/admin/StaffInfomation.tsx
@@ -0,0 +1,72 @@
+import { Router } from "../../routers";
+import styles from "./UserSetting.module.css";
+import { FilePen, Trash2 } from "lucide-react";
+
+const StaffInfomation = () => {
+  return (
+    <div>
+      <Router />
+      <div>
+        <div className={styles.container}>
+          <div>
+            <h1>User Settings</h1>
+            <hr />
+          </div>
+          <div
+            style={{
+              overflow: "hidden",
+              marginTop: "20px",
+              boxShadow:
+                "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
+            }}
+          >
+            <table className={styles.table}>
+              <tr>
+                <th className={styles.style_th}>#</th>
+                <th className={styles.style_th}>Name</th>
+                <th className={styles.style_th}>Department</th>
+                <th className={styles.style_th}>Job Rank</th>
+                <th className={styles.style_th}>Salary</th>
+                <th className={styles.style_th}>Action</th>
+              </tr>
+              <tr>
+                <td className={styles.style_td}>01</td>
+                <td className={styles.style_td}>Josh</td>
+                <td className={styles.style_td}>Text</td>
+                <td className={styles.style_td}>Text</td>
+                <td className={styles.style_td}>Number</td>
+                <td className={styles.style_td}>
+                  <FilePen /> <Trash2 />
+                </td>
+              </tr>
+              <tr>
+                <td className={styles.style_td}>01</td>
+                <td className={styles.style_td}>Josh</td>
+                <td className={styles.style_td}>Text</td>
+                <td className={styles.style_td}>Text</td>
+                <td className={styles.style_td}>Number</td>
+                <td className={styles.style_td}>
+                  <FilePen /> <Trash2 />
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div
+            style={{
+              marginTop: "20px",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <button className={styles.button2}>Previous</button>
+            <button className={styles.button3} style={{ marginLeft: "10px" }}>
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StaffInfomation;
---
+++ hcm25-react-02-team-01-fe-feature-admin-userSetting/Desktop/claim_request_FE/src/types/button.type.ts
@@ -0,0 +1,12 @@
+export type ButtonProps = {
+  width?: number;
+  height?: number;
+  color?: string;
+  name: string;
+  backgroundColor?: string;
+  borderRadius?: number;
+  padding?: string;
+  margin?: string;
+  border?: string;
+  onClick?: () => void;
+};
