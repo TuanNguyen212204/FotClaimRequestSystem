d72457f6aae9a8849e849b9d465d6d0932d1e0b5
Nguyen Tuan An <anntse173412@fpt.edu.vn>
2025-02-16 20:27:43 +0700

    sử dụng custom hook

--- Desktop/claim_request_FE/src/components/ui/Forms/Claim/CreateClaim.tsx
+++ Desktop/claim_request_FE/src/components/ui/Forms/Claim/CreateClaim.tsx
@@ -1,64 +1,16 @@
-import { useState } from "react";
-import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, FormProvider } from "react-hook-form";
 import InputField from "../Inputs/InputField";
-
 import {
-  createRequestFormSchema,
+  useCreateClaimForm,
   RequestFormValues,
-} from "../../../../constant/Zod_Schema/Forms/createRequest";
+} from "../../../../Hooks/useCreateClaimForm";
+import styles from '../Claim/Claim.module.css'
 import { FaCalendar } from "react-icons/fa";
 export default function CreateClaim() {
-  const [minDate, setMinDate] = useState("2025-01-01");
-  const [maxDate, setMaxDate] = useState("2025-12-31");
+  const { methods, errors, computedOvertimeDuration, computedTotalHours } =
+    useCreateClaimForm();
 
-  const schema = createRequestFormSchema(minDate, maxDate);
-
-  const methods = useForm<RequestFormValues>({
-    resolver: zodResolver(schema),
-    mode: "all",
-    defaultValues: {
-      from: "",
-      to: "",
-      workingHoursFrom: "17:00",
-      workingHoursTo: "23:59",
-    },
-  });
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-    watch,
-  } = methods;
-
-  const fromDate = watch("from");
-  const toDate = watch("to");
-  const workingHoursFrom = watch("workingHoursFrom");
-  const workingHoursTo = watch("workingHoursTo");
-
-  let computedOvertimeDuration = "";
-  if (fromDate && toDate) {
-    const date1 = new Date(fromDate);
-    const date2 = new Date(toDate);
-    const diffTime = Math.abs(date2.getTime() - date1.getTime());
-    const diffDays = diffTime / (1000 * 60 * 60 * 24);
-    computedOvertimeDuration = diffDays.toFixed(0);
-  }
-
-  let computedTotalHours = "";
-  if (workingHoursFrom && workingHoursTo) {
-    const [startHour, startMinute] = workingHoursFrom.split(":").map(Number);
-    const [endHour, endMinute] = workingHoursTo.split(":").map(Number);
-    const startTotal = startHour * 60 + startMinute;
-    let endTotal = endHour * 60 + endMinute;
-    if (endTotal < startTotal) {
-      endTotal += 24 * 60;
-    }
-    const diffHours = (endTotal - startTotal) / 60;
-    computedTotalHours = Math.round(
-      Number.parseInt(diffHours.toFixed(2))
-    ).toString();
-  }
+  const { handleSubmit, register } = methods;
 
   const onSubmit: SubmitHandler<RequestFormValues> = (data) => {
     console.log("Valid form data:", data);
@@ -79,7 +31,7 @@ export default function CreateClaim() {
             className="p-2 border border-gray-400 rounded-lg bg-white text-black"
           /> */}
           <span className="text-black font-extrabold">
-            {computedOvertimeDuration}
+            {computedOvertimeDuration === "" ? "0" : computedOvertimeDuration}
           </span>
         </div>
 
@@ -137,7 +89,7 @@ export default function CreateClaim() {
         <div className="mt-6 text-center">
           <button
             type="submit"
-            className="px-6 py-3 hover:bg-orange-400 bg-orange-500 text-white font-semibold text-center rounded-lg"
+            className={`px-6 py-3 text-white font-semibold text-center rounded-lg ${styles.submit_button }` }
           >
             Send
           </button>
