db73dabdcb9d3f6f6371fd3ef43fdbb5cf487d74
tri <trihqse170376@fpt.edu.vn>
2025-03-11 16:03:31 +0700

    Loading full screen

--- Desktop/claim_request_FE/src/main.tsx
+++ Desktop/claim_request_FE/src/main.tsx
@@ -6,14 +6,18 @@ import { Provider } from "react-redux";
 import { store } from "./redux/index.ts";
 import { BrowserRouter } from "react-router-dom";
 import { ReactToastifyProvider } from "./context/ReactToastifyProvider.tsx";
-
+import { LoadingProvider } from "./components/common/LoadingFullScreen/LoadingContext.tsx";
+import LoadingOverlay from "./components/common/LoadingFullScreen/LoadingOverlay.tsx";
 createRoot(document.getElementById("root")!).render(
   <ReactToastifyProvider>
     <BrowserRouter>
       <Provider store={store}>
-        <ReactQueryProvider>
-          <App />
-        </ReactQueryProvider>
+      <LoadingProvider>
+            
+              <LoadingOverlay /> {/* Full-screen overlay */}
+              <App />
+            
+          </LoadingProvider>
       </Provider>
     </BrowserRouter>
   </ReactToastifyProvider>
