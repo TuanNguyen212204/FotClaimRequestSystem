9c5432bb12e5f0fd77a7c9ad3cdada781828b10f
tri <trihqse170376@fpt.edu.vn>
2025-03-11 16:20:07 +0700

    loading

--- Desktop/claim_request_FE/src/main.tsx
+++ Desktop/claim_request_FE/src/main.tsx
@@ -7,15 +7,19 @@ import { store } from "./redux/index.ts";
 import { BrowserRouter } from "react-router-dom";
 import { ReactToastifyProvider } from "./context/ReactToastifyProvider.tsx";
 import { NotificationProvider } from "./components/ui/Notification/NotificationContext.tsx";
-
+import { LoadingProvider } from "./components/common/LoadingFullScreen/LoadingContext.tsx";
+import LoadingOverlay from "./components/common/LoadingFullScreen/LoadingOverlay.tsx";
 createRoot(document.getElementById("root")!).render(
   <ReactToastifyProvider>
     <BrowserRouter>
       <Provider store={store}>
         <ReactQueryProvider>
-          <NotificationProvider>
-                      <App />
-          </NotificationProvider>
+        <LoadingProvider>
+            <NotificationProvider>
+              <LoadingOverlay /> {/* Full-screen overlay */}
+              <App />
+            </NotificationProvider>
+          </LoadingProvider>
         </ReactQueryProvider>
       </Provider>
     </BrowserRouter>
