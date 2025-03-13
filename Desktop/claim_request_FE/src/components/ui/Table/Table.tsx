3ed4be190f802b51ef68cfeaa74bb486fa42b47d
Tran Anh Vy <trananhvy9420@gmail.com>
2025-03-13 09:34:09 +0700

    Vỹ Trần thêm onChange trong input để đỡ bị console log báo đỏ

--- Desktop/claim_request_FE/src/components/ui/Table/Table.tsx
+++ Desktop/claim_request_FE/src/components/ui/Table/Table.tsx
@@ -266,6 +266,7 @@ const TableComponent = forwardRef(
                           type="checkbox"
                           checked={checkedItems.size === dataSource.length}
                           value=""
+                          onChange={handleSelectAll}
                           className=" text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 "
                         />
                       </div>
