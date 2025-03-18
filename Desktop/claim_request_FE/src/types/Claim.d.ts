91b2530b85cc539e83d00d7d33f8f98a9b0565d2
HenryNguyen30 <trinc0201@gmail.com>
2025-03-18 15:05:21 +0700

    thêm data vào type

--- Desktop/claim_request_FE/src/types/Claim.d.ts
+++ Desktop/claim_request_FE/src/types/Claim.d.ts
@@ -18,3 +18,45 @@ export interface PendingClaim {
     working_hours: number;
   }[];
 }
+
+export interface ApprovedClaim {
+  request_id: string;
+  user_id: string;
+  start_date: string;
+  end_date: string;
+  total_hours: number;
+  project_id: string;
+  submitted_date: string;
+  claim_status: string;
+  user: {
+    full_name: string;
+    salary: string;
+    ot_rate: string;
+  };
+  salary_overtime: string;
+  claim_details: {
+    date: string;
+    working_hours: number;
+  }[];
+}
+
+export interface RejectedClaim {
+  request_id: string;
+  user_id: string;
+  start_date: string;
+  end_date: string;
+  total_hours: number;
+  project_id: string;
+  submitted_date: string;
+  claim_status: string;
+  user: {
+    full_name: string;
+    salary: string;
+    ot_rate: string;
+  };
+  salary_overtime: string;
+  claim_details: {
+    date: string;
+    working_hours: number;
+  }[];
+}
