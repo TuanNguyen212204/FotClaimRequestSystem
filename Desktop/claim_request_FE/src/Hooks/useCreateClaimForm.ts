3d8c226b41419af04f20d76dce1c2b8c7c61efab
Nguyen Tuan An <anntse173412@fpt.edu.vn>
2025-03-25 17:25:24 +0700

    refractor create claim page có thể dùng để view hoặc update claims

--- Desktop/claim_request_FE/src/Hooks/useCreateClaimForm.ts
+++ Desktop/claim_request_FE/src/Hooks/useCreateClaimForm.ts
@@ -4,13 +4,22 @@ import { formSchema } from "@/Schemas/ClaimSchema";
 import { FormData } from "@/types/claimForm.type";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "@/redux";
-import { createClaim } from "@/redux/thunk/CreateClaim";
+import { createClaim, updateClaim } from "@/redux/thunk/CreateClaim";
 import { selectUserById } from "@/redux/selector/userSelector";
 import { CreateClaimData } from "@/Services/Project/Project.type";
 import { toast } from "react-toastify";
 import { ApiError } from "@/api";
-
-export default function useCreateClaimForm() {
+import { useEffect } from "react";
+interface CreateClaimFormProps {
+  initialValues?: FormData;
+  mode: "create" | "view" | "update";
+  requestID?: string;
+}
+export default function useCreateClaimForm({
+  initialValues,
+  mode,
+  requestID,
+}: CreateClaimFormProps) {
   const {
     register,
     control,
@@ -20,11 +29,12 @@ export default function useCreateClaimForm() {
     setValue,
     reset,
     setError,
+    watch,
     clearErrors,
     trigger,
   } = useForm<FormData>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
+    defaultValues: initialValues || {
       currentSelectedProject: {
         projectID: "",
         projectName: "",
@@ -35,6 +45,18 @@ export default function useCreateClaimForm() {
     },
     mode: "all",
   });
+  const formValues = watch("claims");
+  useEffect(() => {
+    console.log("Form Values:", formValues);
+    console.log("Errors:", errors);
+    // if (Object.keys(errors).length > 0) {
+    //   console.log("Validation Errors:", errors);
+    // }
+    if (Object.keys(formState.errors).length > 0) {
+      console.log("Validation Errors:", formState.errors);
+      trigger();
+    }
+  }, [JSON.stringify(formValues)]);
 
   const dispatch = useDispatch<AppDispatch>();
   const user = useSelector(selectUserById);
@@ -43,6 +65,7 @@ export default function useCreateClaimForm() {
     console.log(data);
     console.log(errors);
 
+    console.log("Validation errors", formState.errors);
     const isValid = await trigger();
 
     if (!data.currentSelectedProject.projectID) {
@@ -70,26 +93,56 @@ export default function useCreateClaimForm() {
       projectID: data.currentSelectedProject.projectID,
       claims: data.claims,
     };
-
-    try {
-      const resultAction = await dispatch(createClaim(DataToSend));
-      if (createClaim.fulfilled.match(resultAction)) {
-        toast.success("Claim request created successfully");
-        reset();
-      } else {
-        const payloadError = resultAction.payload as any;
-        const errorMessage = payloadError?.message || "Unknown error occurred";
-        toast.error(`Failed to create claim: ${errorMessage}`);
+    if (mode === "create") {
+      try {
+        const resultAction = await dispatch(createClaim(DataToSend));
+        if (createClaim.fulfilled.match(resultAction)) {
+          toast.success("Claim request created successfully");
+          reset();
+        } else {
+          const payloadError = resultAction.payload as any;
+          const errorMessage =
+            payloadError?.message || "Unknown error occurred";
+          toast.error(`Failed to create claim: ${errorMessage}`);
+        }
+      } catch (error) {
+        if (error instanceof ApiError) {
+          console.log("API Error:", error.message, error.status, error.data);
+        } else if (error instanceof Error) {
+          console.log("Standard Error:", error.message);
+        } else {
+          console.log("Unknown Error:", error);
+        }
+        toast.error("Failed to create claim request");
       }
-    } catch (error) {
-      if (error instanceof ApiError) {
-        console.log("API Error:", error.message, error.status, error.data);
-      } else if (error instanceof Error) {
-        console.log("Standard Error:", error.message);
-      } else {
-        console.log("Unknown Error:", error);
+    } else if (mode === "update") {
+      try {
+        if (!requestID) {
+          toast.error("No requestID");
+          return;
+        }
+        const resultAction = await dispatch(
+          updateClaim({ claimData: DataToSend, requestID: requestID })
+        );
+        if (updateClaim.fulfilled.match(resultAction)) {
+          toast.success("Claim request updated successfully");
+          reset();
+        } else {
+          const payloadError = resultAction.payload as any;
+          const errorMessage =
+            payloadError?.message || "Unknown error occurred";
+          toast.error(`Failed to update claim: ${errorMessage}`);
+        }
+      } catch (error) {
+        if (error instanceof ApiError) {
+          console.log("API Error:", error.message, error.status, error.data);
+        } else if (error instanceof Error) {
+          console.log("Standard Error:", error.message);
+        } else {
+          console.log("Unknown Error:", error);
+        }
+        toast.error("Failed to update claim request");
       }
-      toast.error("Failed to create claim request");
     }
   };
 
--- Desktop/claim_request_FE/src/pages/CreateClaim/Components/Body/ButtonGroup.tsx
+++ Desktop/claim_request_FE/src/pages/CreateClaim/Components/Body/ButtonGroup.tsx
@@ -2,20 +2,42 @@ import styles from "../../Claim.module.css";
 import { JSX } from "react";
 import { isClaimCreationLoading } from "@/redux/slices/Project/projectSlice";
 import { useSelector } from "react-redux";
-export default function ButtonGroup(): JSX.Element {
+interface ButtonGroupProps {
+  mode: "create" | "view" | "update";
+}
+export default function ButtonGroup({ mode }: ButtonGroupProps): JSX.Element {
   const loading = useSelector(isClaimCreationLoading);
   return (
     <div className={styles.actions}>
       {/* <button type="button" className={`${styles.btn} ${styles.btn_secondary}`}>
         Send
       </button> */}
-      <button
+      {/* <button
         disabled={loading}
         type="submit"
         className={`${styles.btn} ${styles.btn_primary}`}
       >
         Send
-      </button>
+      </button> */}
+      {mode === "create" && (
+        <button
+          disabled={loading}
+          type="submit"
+          className={`${styles.btn} ${styles.btn_primary}`}
+        >
+          Send
+        </button>
+      )}
+      {mode === "update" && (
+        <button
+          disabled={loading}
+          type="submit"
+          className={`${styles.btn} ${styles.btn_primary}`}
+        >
+          Update
+        </button>
+      )}
+      {mode === "view" && <></>}
     </div>
   );
 }
