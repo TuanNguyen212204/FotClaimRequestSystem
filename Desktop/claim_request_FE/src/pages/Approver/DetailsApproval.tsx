de88d577e201b9f93de81566b5e496f634992768
HenryNguyen30 <trinc0201@gmail.com>
2025-03-25 20:07:34 +0700

    fix detail style

--- Desktop/claim_request_FE/src/pages/Approver/DetailsApproval.tsx
+++ Desktop/claim_request_FE/src/pages/Approver/DetailsApproval.tsx
@@ -110,7 +110,7 @@ export const DetailsApproval: React.FC<PendingDetailModalProps> = ({
             <p className={styles.username}>{claimDetail?.user.full_name}</p>
           </div>
           <div className={styles.emailContainer}>
-            <Mail className={styles.emailIcon} />|
+            <Mail className={styles.emailIcon} />
             <p className={styles.email}>{claimDetail?.user.email}</p>
           </div>
           <hr className={styles.divider} />
@@ -127,33 +127,24 @@ export const DetailsApproval: React.FC<PendingDetailModalProps> = ({
                 {claimDetail?.project_name}
               </span>
             </div>
-            <div>
-              <div className={styles.timeDuration}>
-                <p>
-                  <strong>Time Duration:</strong>
-                </p>
-                <h4>
-                  <span>
-                    {formatDateToMonthDay(`${claimDetail?.start_date}`)}
-                  </span>
-                  <MoveRight size={20} className={styles.iconMoveRight} />
-                  <span>
-                    {formatDateToMonthDay(`${claimDetail?.end_date}`)}
-                  </span>
-                </h4>
-              </div>
-              <p>
-                <strong>Submitted Date:</strong>
-                <span>
-                  {formatDateToMonthDay(`${claimDetail?.submitted_date}`)}
-                </span>
-              </p>
-              <p>
-                <strong>Total Working Hours:</strong>
-                <span>{claimDetail?.total_hours} hours</span>
-              </p>
-              <p>
-                <strong>Status:</strong>
+
+            <div className={styles.projectRow}>
+              <span className={styles.projectLabel}>Time Duration:</span>
+              <span className={styles.projectValue}>
+                {formatDateToMonthDay(`${claimDetail?.start_date}`)}
+                <MoveRight size={20} className={styles.iconMoveRight} />
+                {formatDateToMonthDay(`${claimDetail?.end_date}`)}
+              </span>
+            </div>
+            <div className={styles.projectRow}>
+              <span className={styles.projectLabel}>Submitted Date:</span>
+              <span className={styles.projectValue}>
+                {formatDateToMonthDay(`${claimDetail?.submitted_date}`)}
+              </span>
+            </div>
+            <div className={styles.projectRow}>
+              <span className={styles.projectLabel}>Status:</span>
+              <span className={styles.projectValue}>
                 {claimDetail?.claim_status ? (
                   <StatusTag
                     status={
@@ -167,29 +158,41 @@ export const DetailsApproval: React.FC<PendingDetailModalProps> = ({
                 ) : (
                   "-"
                 )}
-              </p>
+              </span>
+            </div>
+            <div className={styles.projectRow}>
+              <span className={styles.projectLabel}>Total Working Hours:</span>
+              <span className={styles.projectValue}>
+                {claimDetail?.total_hours} hours
+              </span>
             </div>
           </div>
-          {claimDetail?.claim_details &&
-            claimDetail.claim_details.length > 0 && (
+          <div className={styles.containerHistory}>
+            {claimDetail?.claim_details &&
+            claimDetail.claim_details.length > 0 ? (
               <div className={styles.history}>
-                <span style={{ fontWeight: "500", fontSize: "20px" }}>
-                  Claim History
-                </span>
+                <p>History</p>
                 {claimDetail.claim_details.map((detail, index) => (
                   <div key={index} className={styles.historyItem}>
-                    <p>
-                      <strong>Date:</strong>
-                      <span>{formatDateToMonthDay(detail.date)}</span>
-                    </p>
-                    <p>
-                      <strong>Working Hours:</strong>
-                      <span>{detail.working_hours} hours</span>
-                    </p>
+                    <span className={styles.historyItemDate}>
+                      {formatDateToMonthDay(detail.date)}
+                    </span>
+
+                    <div className={styles.historyItemInfo}>
+                      <div className={styles.historyItemRow}>
+                        <span className={styles.historyItemLabel}>
+                          Working Hours:
+                        </span>
+                        <span className={styles.historyItemValue}>
+                          {detail.working_hours} hours
+                        </span>
+                      </div>
+                    </div>
                   </div>
                 ))}
               </div>
-            )}
+            ) : null}
+          </div>
         </div>
       </div>
     </Modal>
