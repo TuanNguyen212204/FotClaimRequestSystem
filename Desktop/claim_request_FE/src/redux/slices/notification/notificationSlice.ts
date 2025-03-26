5475da6b8d9cca54a9031ca3b8f5fa2e810cc28c
khailuu <khailuu123@gmail.com>
2025-03-26 09:28:46 +0700

    fix notification slice

--- Desktop/claim_request_FE/src/redux/slices/notification/notificationSlice.ts
+++ Desktop/claim_request_FE/src/redux/slices/notification/notificationSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { fetchNotificationsAsync, markNotificationAsRead } from "@/redux/thunk/notification/notificationThunk";
 
 interface Notification {
