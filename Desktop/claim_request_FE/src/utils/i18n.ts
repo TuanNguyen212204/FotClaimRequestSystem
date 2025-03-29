bf2253f4c0d78a8eaeb1d040ab66e1ac3554800d
Duy Anh <anhthdse172815@fpt.edu.vn>
2025-03-29 13:24:14 +0700

    chỉnh

--- Desktop/claim_request_FE/src/utils/i18n.ts
+++ Desktop/claim_request_FE/src/utils/i18n.ts
@@ -6,11 +6,6 @@ import headerEn from "../locales/en/header.json";
 import headerVn from "../locales/vi/header.json";
 import userInfoEn from "../locales/en/userInfo.json";
 import userInfoVn from "../locales/vi/userInfo.json";
-// Khởi tạo i18n
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { header: headerEn, userInfo: userInfoEn },
-    vi: { header: headerVn, userInfo: userInfoVn },
 import financeEn from "../locales/en/finance.json";
 import financeVn from "../locales/vi/finance.json";
 import approvedetailEn from "../locales/en/approvedetail.json";
@@ -20,11 +15,11 @@ import paidclaimsVi from "../locales/vi/paidclaims.json";
 import claimstatusEn from "../locales/en/claimstatus.json";
 import claimstatusVi from "../locales/vi/claimstatus.json";
 
-// Khởi tạo i18n
 i18n.use(initReactI18next).init({
   resources: {
     en: { 
       header: headerEn,
+      userInfo: userInfoEn,
       finance: financeEn,
       approvedetail: approvedetailEn,
       paidclaims: paidclaimsEn,
@@ -32,15 +27,16 @@ i18n.use(initReactI18next).init({
     },
     vi: { 
       header: headerVn,
+      userInfo: userInfoVn,
       finance: financeVn,
       approvedetail: approvedetailVi,
       paidclaims: paidclaimsVi,
       claimstatus: claimstatusVi
     },
   },
-  lng: "en", // Ngôn ngữ mặc định
+  lng: "en",
   fallbackLng: "vi",
-  ns: ["header", "finance", "approvedetail", "paidclaims", "claimstatus"],
+  ns: ["header", "userInfo", "finance", "approvedetail", "paidclaims", "claimstatus"],
   defaultNS: "header",
   debug: true,
   interpolation: {
